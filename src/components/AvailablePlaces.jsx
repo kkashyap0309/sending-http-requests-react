50db68e6cb40bbb00f2980e5466dcd9835247b75
kunal <kashyap.kunal123@gmail.com>
2025-07-06 07:43:05 +0530

    available places sorting based on location and moved the http calls to httpUtils.js

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
-import ErrorPage from "./Error.jsx"; 
+import ErrorPage from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { getAvailablePlaces } from "../httpUtil.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [dataLoading, setDataLoading] = useState(false);
-  const [err ,  setErr] = useState();
+  const [err, setErr] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       // we can add a function inside the useEffect hook to wait for the result synchronously
       setDataLoading(true);
       try {
-        const response = await fetch("http://localhost:3000/placesss");
-        const resData = await response.json();
-        if (!response.ok) {
-          throw new Error ();
-        }
-        setAvailablePlaces(resData.places);
+        const places = await getAvailablePlaces();
+        //getting user location to sort the places by distance by using javascript navigator method
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setDataLoading(false);
+        });
       } catch (error) {
-        setErr({msg: error.message || "Could not fetch the details try again !!!"})
+        setErr({
+          msg: error.message || "Could not fetch the details try again !!!",
+        });
+        setDataLoading(false);
       }
-      setDataLoading(false);
     }
 
     fetchPlaces(); //then call the above function
   }, []);
 
   if (err) {
-    return <ErrorPage title="Something went wrong" message={err.msg} />
+    return <ErrorPage title="Something went wrong" message={err.msg} />;
   }
 
   return (
